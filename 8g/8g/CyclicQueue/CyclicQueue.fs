0e2ec9f7470e9167d2383e7a69704b4aea5e1e3f
Husted42 <115221407+Husted42@users.noreply.github.com>
2022-12-03 13:29:53 +0100

    head=first tail=last

--- 8g/8g/CyclicQueue/CyclicQueue.fs
+++ 8g/8g/CyclicQueue/CyclicQueue.fs
@@ -5,15 +5,15 @@ type Value = int
 
 //Variables
 let mutable q = [||]
-let mutable tail = -1
-let mutable head = 0
+let mutable last = -1
+let mutable first = 0
 let mutable size = 0
 
 //Functions
 /// <summary>Create or clear the cyclic queue</summary>
 /// <param name="n">The maximum number of elements</param>
 let create (n: int) : unit =
-    tail <- -1
+    last <- -1
     q <- [||]
     q <- Array.append q [|for i in 1 .. n -> None|]
     printfn "array: %A" q
@@ -25,35 +25,35 @@ let enqueue (e: Value) : bool =
     if size = q.Length
     then false
     else
-        if tail = (q.Length-1)
+        if last = (q.Length-1)
         then
             size <- size + 1
-            tail <- 0 
-            q[tail] <- (Some e)
+            last <- 0 
+            q[last] <- (Some e)
             true
         else 
             size <- size + 1
-            tail <- tail + 1 
-            q[tail] <- (Some e)
+            last <- last + 1 
+            q[last] <- (Some e)
             true
  
 /// <summary>Remove the element in the front position of the queue</summary>
-/// <returns>The first element in q or None if the queue is empty</returns>
+/// <returns>The last element in q or None if the queue is empty</returns>
 let dequeue () : Value option =
     if size = 0
     then None
     else
-        if head = (q.Length-1)
+        if first = (q.Length-1)
         then
             size <- size - 1
-            head <- 0
-            let h = head
-            head  <- head + 1
+            first <- 0
+            let h = first
+            first  <- first + 1
             q[h]
         else
             size <- size - 1
-            let h = head
-            head  <- head + 1
+            let h = first
+            first  <- first + 1
             q[h]
         
 /// <summary>Check if the queue is empty</summary>
@@ -72,7 +72,7 @@ let length () : int =
 /// <returns>A string representing the queue's elements</returns>
 let toString () : string =
     let mutable str = ""
-    let mutable h = head
+    let mutable h = first
     for i = 0 to (size-1) do
         if h = (q.Length-1)
         then 
