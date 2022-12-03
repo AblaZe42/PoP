c62bcae93f9a3f3419874caab07ef04f07626583
Husted42 <115221407+Husted42@users.noreply.github.com>
2022-12-03 15:38:52 +0100

    lastStuff

--- 8g/8g/CyclicQueue/CyclicQueue.fs
+++ 8g/8g/CyclicQueue/CyclicQueue.fs
@@ -10,17 +10,12 @@ let mutable first = 0
 let mutable size = 0
 
 //Functions
-/// <summary>Create or clear the cyclic queue</summary>
-/// <param name="n">The maximum number of elements</param>
 let create (n: int) : unit =
     last <- -1
     q <- [||]
     q <- Array.append q [|for i in 1 .. n -> None|]
     printfn "array: %A" q
 
-/// <summary>Add an element to the end of a queue</summary>
-/// <param name="e">an element</param>
-/// <returns>True if the queue had space for the element</returns>
 let enqueue (e: Value) : bool =
     if size = q.Length
     then false
@@ -37,8 +32,6 @@ let enqueue (e: Value) : bool =
             q[last] <- (Some e)
             true
  
-/// <summary>Remove the element in the front position of the queue</summary>
-/// <returns>The last element in q or None if the queue is empty</returns>
 let dequeue () : Value option =
     if size = 0
     then None
@@ -56,20 +49,14 @@ let dequeue () : Value option =
             first  <- first + 1
             q[h]
         
-/// <summary>Check if the queue is empty</summary>
-/// <returns>True if the queue is empty</returns>
 let isEmpty () : bool =
     if size = 0
     then true
     else false   
 
-/// <summary>Get the length of the queue</summary>
-/// <returns>The number of elements in the queue</returns>
 let length () : int =
     size
 
-/// <summary>The queue on string form</summary>
-/// <returns>A string representing the queue's elements</returns>
 let toString () : string =
     let mutable str = ""
     let mutable h = first
