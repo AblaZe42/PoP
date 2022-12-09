136d3d4de532aa526a63147108e09d49375040d0
Husted42 <115221407+Husted42@users.noreply.github.com>
2022-12-09 19:57:29 +0100

    9i0

--- 9i/9i0test1.fsx
+++ 9i/9i0test1.fsx
@@ -2,6 +2,8 @@ type drone (start : float*float, dest : float*float, speed : float) =
     let mutable position  = start
     let destination = dest
     let mutable speed = speed
+    member this.pos =
+        position
     member this.fly =
         let route = ((fst destination) - (fst position), (snd destination) - (snd position))
         let routeLength = sqrt(((fst route) ** 2) + ((snd route) ** 2))
@@ -14,17 +16,52 @@ type drone (start : float*float, dest : float*float, speed : float) =
             position
         else
             position
-    member this.aDestination =
+    member this.atDestination =
         if destination = position
         then true
         else false
 
-let drone1 = drone((5.,5.),(10.,10.), 100.)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
-printfn "%A" (drone1.fly)
+type airSpace (drones) = 
+    let mutable drones = []
+    let mutable droneNumber = 1
+    member drone.dist (drone1: float*float, drone2: float*float) = 
+        ((((fst drone2) - (fst drone1))**2.) + (((snd drone2) - (snd drone1))**2.))
+    member this.AddDrone(a, b, c) =
+        let x = [drone(a, b, c)]
+        drones <- drones @ x
+    member this.flyDrones =
+        drones |> List.map (fun x -> x.fly) 
+    member this.Collide()
+        
+
+let airSpace1 = airSpace()
+airSpace1.AddDrone((5.,5.),(10.,10.), 100.)
+airSpace1.AddDrone((4.,4.),(14.,14.), 100.)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+printfn "%A" (airSpace1.flyDrones)
+
+// printfn "   "
+// printfn "droneTest"
+// let drone1 = drone((0.,50.),(50.,50.), 100.)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+// printfn "%A" (drone1.fly)
+
+// printfn "%A" (drone1.atDestination)
